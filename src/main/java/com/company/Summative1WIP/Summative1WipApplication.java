ee6a6b5c03507b33f3836e50e28ff74061b9abb0
treyduffus <105085785+treyduffus@users.noreply.github.com>
2023-02-03 18:29:47 -0500

    First commit

---
+++ src/main/java/com/company/Summative1WIP/Summative1WipApplication.java
@@ -0,0 +1,13 @@
+package com.company.Summative1WIP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Summative1WipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Summative1WipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/Summative1WIP/Summative1WipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Summative1WIP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Summative1WipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
